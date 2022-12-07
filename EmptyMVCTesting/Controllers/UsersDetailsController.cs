c27817aaeead68077568dcad009b0022bb7e98a6
NIL\AnilGawade <anilgawade38@gmail.com>
2022-12-08 03:37:52 +0530

    chg by Anil

--- EmptyMVCTesting/Controllers/UsersDetailsController.cs
+++ EmptyMVCTesting/Controllers/UsersDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,9 +20,46 @@ namespace EmptyMVCTesting.Controllers
 
         [HttpPost]
         [Route("DocUpload")]
-        public string PhotoUpload()
+        public ActionResult PhotoUpload()
         {
-            return "True";
+            if(Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    for(int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+
+                        // Get the complete folder path and store the file inside it.  
+                        fname = Path.Combine(Server.MapPath("~/Docs/UploadsDocs/"), fname);
+                        file.SaveAs(fname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json("File Uploaded Successfully!");
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+                return Json("No files selected.");
+            //Resquest
+            //return "True";
         }
 
         [Route("CreateUser")]
@@ -39,6 +77,20 @@ namespace EmptyMVCTesting.Controllers
             UserDetails UD = new UserDetails();
 
             return View("RegisterUser", UD);
+        }
+
+        [HttpPost]
+        [Route("RegisterUser")]
+        public ActionResult CreateUser(FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                var U_FName = form["U_FName"];
+                var U_Gen = form["U_Gen"];
+                var U_Hobbies = form["U_Hobbies"];
+                var HobbyList = form["HobbyList"];
+            }
+            return View("RegisterUser");
         }
     }
 }
