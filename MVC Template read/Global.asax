0fd885a17c834ceb7309b8d54983d3a9a42f6e6b
Nil <anilgawade38@gmail.com>
2022-12-22 03:17:03 +0530

    MVC Ready Template Added by Anil New Project

---
+++ MVC Template read/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Template_read.MvcApplication" Language="C#" %>
---
+++ MVC Template read/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Template_read.Startup))]
+namespace MVC_Template_read
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
